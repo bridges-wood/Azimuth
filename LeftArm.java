0f91fdd307a95a3e7b23c397f8ade1cd59b13376
bridges-wood <45072525+bridges-wood@users.noreply.github.com>
2018-11-15 20:17:00 +0000

    Add files via upload
    Contains character related classes and objects.

---
+++ LeftArm.java
@@ -0,0 +1,8 @@
+
+public class LeftArm extends Apparel {
+
+	public LeftArm(String name, String description, int weight, int value, int[] resistances, int[] REPLICASmodifiers, int damageThreshold) {
+		super(name, description, null, weight, value, false, 2, resistances, REPLICASmodifiers, damageThreshold);
+	}
+
+}
---
+++ RightLeg.java
@@ -0,0 +1,8 @@
+
+public class RightLeg extends Apparel {
+
+	public RightLeg(String name, String description, int weight, int value, int[] resistances, int[] REPLICASmodifiers, int damageThreshold) {
+		super(name, description, null, weight, value, false, 5, resistances, REPLICASmodifiers, damageThreshold);
+	}
+
+}
