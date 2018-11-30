26f3bc90248f25af32bdbd8edf5f5ef35427102d
woodmb <woodmb@IT-03.ksc.internal>
2018-11-30 11:24:29 +0000

    Fully Commented Terminal and Started Menu Class

---
+++ Menu.java
@@ -0,0 +1,17 @@
+
+public class Menu {
+	
+	public Menu() {
+		int choice;
+		while(choice <= 5 && choice <= 1) {
+		System.out.println("MENU");
+		System.out.println("1. New Game");
+		System.out.println("2. Load Game");
+		System.out.println("3. Options");
+		System.out.println("4. Credits");
+		System.out.println("5. Quit");
+		String in = Utilities.StrInput();
+		//choice = String.;
+		}
+	}
+}
--- Terminal.java
+++ Terminal.java
@@ -35,14 +35,15 @@ public class Terminal extends Object {
 	
 	/**
 	 * @param difficulty the level of encryption on the system that affects the difficulty of the hacking process.
+	 * @param hackingSkill the skill of the level of the player.
 	 */
-	public void hack(int difficulty) {
+	public void hack(int difficulty, int hackingSkill) {
 		int dim = (int) Math.ceil((double) (1/difficulty * 10)); /*Calculates the dimensions of the array as ten 
 		 														   times the reciprocal of the difficulty. This
 		 														   is then rounded up to avoid 0 length arrays.*/
-		int guesses = (int) (dim*dim * 0.05); /*Guesses are equal 5% of the number of passwords in the grid. 
-											    TODO determine the application of hacking skills to the 
-											    number of guesses*/
+		
+		int guesses = (int) ((dim*dim * 0.05) + (hackingSkill * 0.05)); /*Guesses are equal 5% of the number of passwords in the grid
+		 																 plus 5% of the players hackingSkill.*/
 		String[][] passwordsPoss = new String[dim][dim];
 		for(int x = 0; x < dim; x++) {
 			for(int y = 0; y < dim; y++) {
@@ -50,20 +51,21 @@ public class Terminal extends Object {
 					passwordsPoss[x][y] = Utilities.generatePassword(difficulty); //Populates the array with random passwords.
 				} catch (FileNotFoundException e) {
 					System.out.println("There has been an error. "
-					+ "This terminal cannot be hacked due to missing resources");
+					+ "This terminal cannot be hacked due to missing resources"); //If the file cannot be found, an error is returned.
 				}
 			}
 		}
-		passwordsPoss[Utilities.rangedRandomInt(0, passwordsPoss[0].length - 1)][Utilities.rangedRandomInt(0, passwordsPoss.length - 1)] = passwords[0];
+		passwordsPoss[Utilities.rangedRandomInt(0, passwordsPoss[0].length - 1)] //Replaces one of the randomly generated passwords in the grid with the true password.
+					 [Utilities.rangedRandomInt(0, passwordsPoss.length - 1)] = passwords[0];
 		for (int x = 0; x < dim; x++) {
 			for (int y = 0; y < dim; y++) {
 				System.out.print(passwordsPoss[x][y] + " ");
 			}
-			System.out.println("");
+			System.out.println(""); //Prints out the 2D array of passwords for the player.
 		}
 		for(int i = 0; i < guesses; i ++) {
 			int sames = 0;
-			String guess = Utilities.StrInput();
+			String guess = Utilities.StrInput(); //Takes in a player's guess at the password.
 			if(!guess.equals(passwords[0])) {
 				if(guess.length() != passwords[0].length()) {
 					System.out.println("That password is not the right length.");
@@ -78,12 +80,13 @@ public class Terminal extends Object {
 				continue;
 			}
 			for(int x = 0; x < locked.length; x++) {
-				this.unLocked(locked, x);
+				this.unLocked(locked, x); //Sets the locked value for all the users to unlocked. 
 			}
 			break;
 		}
 	}
-
+	
+	//Getters and Setters.
 	public String[] getPasswords() {
 		return passwords;
 	}
