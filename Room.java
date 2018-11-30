7a066a364e5818da31a09d2ff802120e334ecfbc
woodmb <woodmb@IT-03.ksc.internal>
2018-11-30 15:43:00 +0000

    Added first getters and setters to room.

--- Room.java
+++ Room.java
@@ -1,4 +1,30 @@
+import java.io.Serializable;
 
-public class Room {
-
+public class Room implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3521702290298779279L;
+	private Object[] contents;
+	private Character[] characters;
+	private String description;
+	
+	public Object[] getContents() {
+		return contents;
+	}
+	public void setContents(Object[] contents) {
+		this.contents = contents;
+	}
+	public Character[] getCharacters() {
+		return characters;
+	}
+	public void setCharacters(Character[] characters) {
+		this.characters = characters;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
