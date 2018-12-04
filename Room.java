bcd83d7a2d434b820c51e93f222a4208178a01e5
woodmb <woodmb@LIB-17.ksc.internal>
2018-12-04 13:50:19 +0000

    Save and Load Routines

--- Room.java
+++ Room.java
@@ -8,7 +8,36 @@ public class Room implements Serializable {
 	private Object[] contents;
 	private Character[] characters;
 	private String description;
+	private int id;
+	private int[] rooms;
+	private boolean[] access;
 	
+	public Room(Object[] contents, Character[] characters, String description, int id, int[] rooms, boolean[] access) {
+		this.contents = contents;
+		this.characters = characters;
+		this.description = description;
+		this.id = id;
+		this.rooms = rooms;
+		this.access = access;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int[] getRooms() {
+		return rooms;
+	}
+	public void setRooms(int[] rooms) {
+		this.rooms = rooms;
+	}
+	public boolean[] getAccess() {
+		return access;
+	}
+	public void setAccess(boolean[] access) {
+		this.access = access;
+	}
 	public Object[] getContents() {
 		return contents;
 	}
